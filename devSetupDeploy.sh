10aaa836af025a7a2fa8fbce758472706296eee7
amireddym <amireddy.m@northeastern.edu>
2022-03-02 21:48:16 -0500

    Updated the blocklevel mapping to override the snapshot initiated one

--- devSetupDeploy.sh
+++ devSetupDeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 aws cloudformation deploy --region us-east-1 --stack-name devVpc --profile dev --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
-EnvironmentName=dev Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 AmiId=ami-060394bb0f9f904e2 ApplicationInstanceType=t2.micro
+EnvironmentName=dev Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 AmiId=ami-069036478e8e52d7b ApplicationInstanceType=t2.micro
 
 aws cloudformation describe-stacks --region us-east-1 --stack-name devVpc --profile dev
