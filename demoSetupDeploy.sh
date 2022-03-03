b99c967d4a3b0db1b31d3fc00e8fa0b1116280fe
amireddym <amireddy.m@northeastern.edu>
2022-03-02 21:55:59 -0500

    Updated demo script with latest AMI

--- demoSetupDeploy.sh
+++ demoSetupDeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 aws cloudformation deploy --stack-name demoVpc --profile demo --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
-EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 AmiId=ami-060394bb0f9f904e2 ApplicationInstanceType=t2.micro
+EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 AmiId=ami-069036478e8e52d7b ApplicationInstanceType=t2.micro
 
 aws cloudformation describe-stacks --stack-name demoVpc --profile demo
