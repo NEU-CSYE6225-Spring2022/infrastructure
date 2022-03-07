1362e8298306a53b673940c91e8b7f324b49d8e6
amireddym <amireddy.m@northeastern.edu>
2022-03-07 15:09:19 -0500

    Removed command to delete S3 bucket. Since it can be deleted by CloudformationStack

--- demoSetupTearDown.sh
+++ demoSetupTearDown.sh
@@ -4,6 +4,6 @@
 aws s3 rm s3://imagedatacsye --recursive --region=us-east-1 --profile=demo
 
 #Removing bucket
-aws s3 rb s3://imagedatacsye --region=us-east-1 --profile=demo
+#aws s3 rb s3://imagedatacsye --region=us-east-1 --profile=demo
 
 aws cloudformation delete-stack --stack-name demoVpc --profile demo
--- devSetupTearDown.sh
+++ devSetupTearDown.sh
@@ -4,6 +4,6 @@
 aws s3 rm s3://imagedatacsye --recursive --region=us-east-1 --profile=dev
 
 #Removing bucket
-aws s3 rb s3://imagedatacsye --region=us-east-1 --profile=dev
+#aws s3 rb s3://imagedatacsye --region=us-east-1 --profile=dev
 
 aws cloudformation delete-stack --stack-name devVpc --profile dev --region us-east-1
