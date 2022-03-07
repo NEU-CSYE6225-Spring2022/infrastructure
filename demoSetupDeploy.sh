d71a50025ee0f58c3d72e238003fa3ec695b9bc2
amireddym <amireddy.m@northeastern.edu>
2022-03-07 14:10:02 -0500

    Added changes for Asssignment 5

--- demoSetupDeploy.sh
+++ demoSetupDeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 aws cloudformation deploy --stack-name demoVpc --profile demo --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
-EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 AmiId=ami-069036478e8e52d7b ApplicationInstanceType=t2.micro
+EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 PrivateSubnet1CIDR=10.0.4.0/24 PrivateSubnet2CIDR=10.0.5.0/24 \
+PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-056a523000736c9a2 ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation describe-stacks --stack-name demoVpc --profile demo
--- demoSetupTearDown.sh
+++ demoSetupTearDown.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -ex
 
+#Removing Objects in the bucket
+aws s3 rm s3://imagedatacsye --recursive --region=us-east-1 --profile=demo
+
+#Removing bucket
+aws s3 rb s3://imagedatacsye --region=us-east-1 --profile=demo
+
 aws cloudformation delete-stack --stack-name demoVpc --profile demo
