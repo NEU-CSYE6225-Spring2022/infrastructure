06c21bfd142a95c71f84a09b0927acb909a677f4
amireddym <amireddy.m@northeastern.edu>
2022-03-09 22:26:13 -0500

    Added random string to generate the bucket name for uniqueness

--- demoSetupDeploy.sh
+++ demoSetupDeploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
 
+#Generating the random String
+randomsTR=$(echo $RANDOM | md5sum | head -c 20)
+
 aws cloudformation deploy --stack-name demoVpc --profile demo --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
 EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 PrivateSubnet1CIDR=10.0.4.0/24 PrivateSubnet2CIDR=10.0.5.0/24 \
-PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-056a523000736c9a2 ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A --capabilities CAPABILITY_NAMED_IAM
+PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-056a523000736c9a2 ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A RandomString=$randomsTR --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation describe-stacks --stack-name demoVpc --profile demo
