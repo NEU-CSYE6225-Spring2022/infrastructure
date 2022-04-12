995e292914e5c29e9877554c088e612e02b0c563
amireddym <amireddy.m@northeastern.edu>
2022-04-12 11:04:59 -0400

    Added all code for yml changes

--- devSetupDeploy.sh
+++ devSetupDeploy.sh
@@ -6,7 +6,7 @@ randomsTR=$(echo $RANDOM | md5sum | head -c 20)
 
 aws cloudformation deploy --region us-east-1 --stack-name devVpc --profile dev --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
 EnvironmentName=dev Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 PrivateSubnet1CIDR=10.0.4.0/24 PrivateSubnet2CIDR=10.0.5.0/24 \
-PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-0455079d0d717d576 ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A RandomString=$randomsTR  \
-HostedZoneDomainName=dev.manojreddya.me. CodeDeployBucketName=codedeploy.dev.manojreddya.me AccountId=974908798416 --capabilities CAPABILITY_NAMED_IAM
+PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-00f1264a58e5b747d ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A RandomString=$randomsTR  \
+HostedZoneDomainName=dev.manojreddya.me. CodeDeployBucketName=codedeploy.dev.manojreddya.me AccountId=974908798416 SEStopicArn=dev.manojreddya.me --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation describe-stacks --region us-east-1 --stack-name devVpc --profile dev
