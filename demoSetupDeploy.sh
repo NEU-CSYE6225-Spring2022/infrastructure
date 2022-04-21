b87e9e54bf0b866a1c825fd4b77d6249023e740f
amireddym <amireddy.m@northeastern.edu>
2022-04-21 13:12:27 -0400

    Added new ami with ssl config

--- demoSetupDeploy.sh
+++ demoSetupDeploy.sh
@@ -12,7 +12,7 @@ randomsTR=$(echo $RANDOM | md5sum | head -c 20)
 
 aws cloudformation deploy --region us-east-1 --stack-name demoVpc --profile demo --template-file ./templates/csye6225-infra.yml --parameter-overrides VpcCIDR=10.0.0.0/16 \
 EnvironmentName=demo Subnet1CIDR=10.0.1.0/24 Subnet2CIDR=10.0.2.0/24 Subnet3CIDR=10.0.3.0/24 PrivateSubnet1CIDR=10.0.4.0/24 PrivateSubnet2CIDR=10.0.5.0/24 \
-PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-0922159300090471a ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A RandomString=$randomsTR \
+PrivateSubnet3CIDR=10.0.6.0/24 AmiId=ami-0da5b703a4eec6557 ApplicationInstanceType=t2.micro DbUser=root DbPassword=a9966995395A RandomString=$randomsTR \
 HostedZoneDomainName=prod.manojreddya.me. CodeDeployBucketName=codedeploy.manojreddya.me AccountId=757570737110 SEStopicArn=prod.manojreddya.me --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation describe-stacks --region us-east-1 --stack-name demoVpc --profile demo
